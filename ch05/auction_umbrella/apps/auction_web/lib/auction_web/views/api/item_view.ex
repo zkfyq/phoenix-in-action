a38e67bde4f75032e8053260139087e658568efd
zhangwei <zwxajh@163.com>
2020-02-24 02:03:28 +0900

    add bids data

--- ch05/auction_umbrella/apps/auction_web/lib/auction_web/views/api/item_view.ex
+++ ch05/auction_umbrella/apps/auction_web/lib/auction_web/views/api/item_view.ex
@@ -6,7 +6,7 @@ defmodule AuctionWeb.Api.ItemView do
   end
 
   def render("show.json", %{item: item}) do
-    %{data: render_one(item, __MODULE__, "item.json")}
+    %{data: render_one(item, __MODULE__, "item_with_bids.json")}
   end
 
   def render("item.json", %{item: item}) do
@@ -16,6 +16,17 @@ defmodule AuctionWeb.Api.ItemView do
       title: item.title,
       description: item.description,
       ends_at: item.ends_at
+    }
+  end
+
+  def render("item_with_bids.json", %{item: item}) do
+    %{
+      type: "item",
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      ends_at: item.ends_at,
+      bids: render_many(item.bids, AuctionWeb.Api.BidView, "bid.json")
     }
   end
 end
